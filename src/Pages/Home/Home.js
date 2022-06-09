aef3ed26f9e89f5862bfcdc5e159816abdf53880
pushpitanandy <nandypushpita@gmail.com>
2022-06-09 20:27:36 +0600

    resume can be downloaded

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
 import developerPhoto from '../../images/Background.png';
+import { saveAs } from "file-saver";
+
 
 const Home = () => {
+
+    const handleDownloadResume = () => {
+        saveAs(process.env.PUBLIC_URL + "/resource/Pushpita Nandy Resume-updated.pdf", "Pushpita Nandy Resume.pdf");
+    }
+
     return (
         <div className='container-fluid m-5 p-5'>
             <Row className='justify-content-center align-items-center mr-4'>
@@ -11,7 +18,7 @@ const Home = () => {
                     <h5>Full Stack Web Developer</h5>
                     <p>Looking for opportunities to develop various Websites</p>
                     <br />
-                    <Button variant="dark">Download My Resume</Button>
+                    <Button onClick={handleDownloadResume} variant="dark">Download My Resume</Button>
                 </Col>
                 <Col xs={12} md={6}>
                     <img src={developerPhoto} alt="" />
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { saveAs } from "file-saver";
 
 const Header = () => {
+
+    const handleDownloadResume = () => {
+        saveAs(process.env.PUBLIC_URL + "/resource/Pushpita Nandy Resume-updated.pdf", "Pushpita Nandy Resume.pdf");
+    }
+
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -15,7 +21,7 @@ const Header = () => {
                             <Nav.Link as={Link} to="/contactMe">Contact Me</Nav.Link>
                         </Nav>
                         <Nav>
-                            <button className='btn btn-link text-light text-decoration-none'>Download Resume</button>
+                            <button onClick={handleDownloadResume} className='btn btn-link text-light text-decoration-none'>Download Resume</button>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
